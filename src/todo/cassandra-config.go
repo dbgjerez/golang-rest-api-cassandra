abde625f04e1bc45e6a1003ed6bd450f44d491d8
David Borrego <dborrego@atsistemas.com>
2019-08-02 19:28:36 +0200

    dynamic keyspace and table creation if not exists

--- src/todo/cassandra-config.go
+++ src/todo/cassandra-config.go
@@ -1,11 +1,42 @@
 package todo
 
-import "github.com/gocql/gocql"
+import (
+	"log"
+	"os"
+
+	"github.com/gocql/gocql"
+)
+
+const (
+	CREATE_KEYSPACE = " CREATE KEYSPACE IF NOT EXISTS " + KEYSPACE_NAME + " WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };"
+	CREATE_TABLE    = "create table if not exists example.todo (id uuid PRIMARY KEY, text text);"
+	KEYSPACE_NAME   = "example"
+)
 
 func InitCluster() *gocql.Session {
-	cluster := gocql.NewCluster("172.17.0.1")
-	cluster.Keyspace = "example"
+	cassandra, exists := os.LookupEnv("CASSANDRA_URL")
+	if !exists {
+		log.Fatal("URL de cassandra no encontrada")
+	} else {
+		log.Println("INFO", "Conectando a Cassandra: "+cassandra)
+	}
+	cluster := gocql.NewCluster(cassandra)
+	createKeyspace(cluster)
+	cluster.Keyspace = KEYSPACE_NAME
 	cluster.Consistency = gocql.Quorum
 	session, _ := cluster.CreateSession()
+	createTodoTable(session)
 	return session
+}
+
+func createKeyspace(cluster *gocql.ClusterConfig) {
+	session, _ := cluster.CreateSession()
+	log.Println("INFO", "Creando keyspace "+KEYSPACE_NAME+" si no existe...")
+	session.Query(CREATE_KEYSPACE).Exec()
+	session.Close()
+}
+
+func createTodoTable(session *gocql.Session) {
+	log.Println("INFO", "Creando tabla si no existe...")
+	session.Query(CREATE_TABLE).Exec()
 }
